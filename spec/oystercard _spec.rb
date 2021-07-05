dc05abff2c0ab325838e8ea8410dcb4be616b337
Matt Firmston <mattfirmston@gmail.com>
2021-07-05 22:18:09 +0100

    added feature to allow use to top up

--- spec/oystercard _spec.rb
+++ spec/oystercard _spec.rb
@@ -1,4 +1,20 @@
 require 'oystercard'
 
 describe Oystercard do
+  describe '#initialize' do
+    it 'gives a new card a balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
+  end
+
+  describe '#top_up' do
+    it "allows user to add money to card" do
+      expect(subject.top_up(10)).to eq(10)
+    end
+
+    it "saves balance to be topped up multiple times" do
+      subject.top_up(10)
+      expect(subject.top_up(10)).to eq(20)
+    end
+  end
 end
