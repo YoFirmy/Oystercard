9162166d40c1cf8b6d7b0fc7d105f6978dc49584
Matt Firmston <mattfirmston@gmail.com>
2021-07-05 22:34:58 +0100

    added feature of maximum limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,18 @@
 class Oystercard
   attr_reader :balance
-
+  MAXIMUM_LIMIT = 90
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Balance is limited to £#{MAXIMUM_LIMIT}" if over_limit?(amount)
     @balance += amount
+  end
+
+  private
+
+  def over_limit?(amount)
+    @balance + amount > MAXIMUM_LIMIT
   end
 end
--- spec/oystercard _spec.rb
+++ spec/oystercard _spec.rb
@@ -8,13 +8,19 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    it "allows user to add money to card" do
+    it 'allows user to add money to card' do
       expect(subject.top_up(10)).to eq(10)
     end
 
-    it "saves balance to be topped up multiple times" do
+    it 'saves balance to be topped up multiple times' do
       subject.top_up(10)
       expect(subject.top_up(10)).to eq(20)
+    end
+
+    it 'raises an error if balance is more than £90' do
+      maximum_limit = Oystercard::MAXIMUM_LIMIT
+      subject.top_up(maximum_limit)
+      expect { subject.top_up(1) }.to raise_error "Balance is limited to £#{maximum_limit}"
     end
   end
 end
