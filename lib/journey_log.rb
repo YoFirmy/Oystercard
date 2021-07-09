83774969b74f707e6f165e80d2d884f633124cd8
Sibel Eren <edaeren575@gmail.com>
2021-07-09 14:57:01 +0100

    first steps of #15

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,2 +1,27 @@
+require_relative 'journey'
+
 class JourneyLog
+
+  attr_reader :current_journey
+
+  def initialize(journey_class: Journey )
+    @journey_class = journey_class
+  end
+
+  def start(station)  
+    @current_journey = @journey_class.new(station)
+  end
+
+  # def finish(station)
+    
+  # end
+
+  private
+  attr_reader :journey_class
+
+  def current_journey 
+    @current_journey ||= journey_class.new
+  end
+
+
 end
